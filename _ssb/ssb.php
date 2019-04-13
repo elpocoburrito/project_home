3a33483cf974a71eeccb27aed66e942e37374907
rockbyo5 <rockbyo5@gmail.com>
2019-04-13 11:44:16 -0400

    Fix

--- _ssb/ssb.php
+++ _ssb/ssb.php
@@ -2,19 +2,17 @@
 <head>
 	<?php include '../head.php';?>
 	<script src="TemplateData/UnityProgress.js"></script>
-    <script src="Build/UnityLoader.js"></script>
-    <script>
-      var gameInstance = UnityLoader.instantiate("gameContainer", "Build/super school brothers webgl 2017-05-08.json", {onProgress: UnityProgress});
-    </script>
+	    <script src="Build/UnityLoader.js"></script>
+	    <script>
+	      var gameInstance = UnityLoader.instantiate("gameContainer", "Build/Pepper kid 2017-07-28.json", {onProgress: UnityProgress});
+	    </script>
 </head>
 <body>
 <?php include '../topdocument_game.php';?>
 			<div class="webgl-content">
 			  <div id="gameContainer"></div>
 			  <div class="footer">
-				<div class="webgl-logo"></div>
-				<div class="fullscreen" onclick="gameInstance.SetFullscreen(1)"></div>
-				<div class="title">Super School Brothers</div>
+				<div class="fullscreen" onclick="gameInstance.SetFullscreen(1)">Super School Brothers</div>
 			  </div>
 			</div>
 <?php include '../bottomdocument.php';?>
